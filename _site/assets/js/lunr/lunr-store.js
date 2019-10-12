46f604cb739169a8543951a7c449795c3281d85e
kuki <bkko@outlook.kr>
2019-10-12 23:05:21 +0900

    config yaml

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"Welcome   Hello world, this is my first Jekyll blog post.   I hope you like it!   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/awesome-first-post/",
+        "teaser":null}]
