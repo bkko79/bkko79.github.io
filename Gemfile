016f607b1600b5902c0143c4bf82894f684c0724
kuki <bkko@outlook.kr>
2019-10-12 23:27:25 +0900

    theme file

--- Gemfile
+++
@@ -1,8 +0,0 @@
-source "https://rubygems.org"
-gem "github-pages", group: :jekyll_plugins
-gem "jekyll-include-cache"
-gem "jekyll-paginate"
-gem "jekyll-sitemap"
-gem "jekyll-gist"
-gem "jekyll-feed"
-gem "jemoji"
--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"Welcome   Hello world, this is my first Jekyll blog post.   I hope you like it!   ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/awesome-first-post/",
-        "teaser":null}]
